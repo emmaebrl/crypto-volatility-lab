f8c3d4a55935b0343cbc848ca7a23e0ee23c2d70
emmaebrl <emmaeberle@hotmail.com>
2025-01-23 16:09:31 +0100

    add results notebook

--- crypto_volatility_lab/portfolio_optimization/portfolioConstructor.py
+++ crypto_volatility_lab/portfolio_optimization/portfolioConstructor.py
@@ -13,19 +13,10 @@ class PortfolioConstructor:
             weights.sum(axis=1), 1
         ), "Weights do not sum to 1 for each time period."
         return weights.values
+    
 
     def risk_parity_weights(self, cov_matrix: np.ndarray) -> np.ndarray:
         inv_diag = np.diag(1 / np.sqrt(np.diag(cov_matrix)))
         weights = inv_diag @ cov_matrix @ inv_diag
         weights /= weights.sum()
-        return weights
-
-    def vol_target_weights(
-        self, volatilities: np.ndarray, target_vol: float
-    ) -> np.ndarray:
-        weights = target_vol / volatilities
-        weights /= weights.sum()
-        assert np.allclose(
-            weights.sum(), 1
-        ), "Weights do not sum to 1 for each time period."
         return weights
