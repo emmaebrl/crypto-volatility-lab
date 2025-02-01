7dbac4e63baa44e4a849cdf6d600a5e56f374595
emmaebrl <emmaeberle@hotmail.com>
2025-02-01 17:35:03 +0100

    push bidon

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.1",
     packages=["crypto_volatility_lab"],
     install_requires=[],
-    description="Tools to analyze and model cryptocurrency volatility.",
+    description="Tools to analyze and model cryptocurrency volatility. Predict future volatility and construct optimal portfolios.",
     author="Lina Benzemma, Sharon Chemmamma, Emma Eberle",
     url="https://github.com/emmaebrl/crypto-volatility-lab",
 )
