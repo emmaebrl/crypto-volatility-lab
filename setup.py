5b2776abdd6c757a8ab2e8e606cd453373d8cca6
emmaebrl <emmaeberle@hotmail.com>
2025-02-01 10:03:03 +0100

    modify setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,8 @@ setup(
     name="crypto_volatility_lab",
     version="0.1",
     packages=["crypto_volatility_lab"],
-    install_requires=[
-        # Liste des dépendances ici si nécessaire, par exemple :
-        # 'numpy>=1.21.0',
-        # 'pandas>=1.3.0',
-    ],
-    description="Un projet pour analyser la volatilité des cryptomonnaies",
-    author="Votre Nom",
-    author_email="votre_email@example.com",
-    url="https://github.com/votre-repo/crypto-volatility-lab",
+    install_requires=[],
+    description="Tools to analyze and model cryptocurrency volatility.",
+    author="Lina Benzemma, Sharon Chemmamma, Emma Eberle",
+    url="https://github.com/emmaebrl/crypto-volatility-lab",
 )
